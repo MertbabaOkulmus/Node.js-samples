7f7522702c0c5d89ff69cd96b43753b69e5a29b3
MertbabaOkulmus <mertbaba.okulmus@hotmail.com>
2021-08-02 11:36:26 +0300

    updated

--- Todo-App/app.js
+++ Todo-App/app.js
@@ -4,19 +4,45 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000
-
-var yapilacaklar=[]
+const uri = "mongodb+srv://nodemongo:123@cluster0.jkyq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.get("/",(req,res)=>{
-    res.render("index",{data:yapilacaklar});
+    MongoClient.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err,db)=>{
+        if(err) throw err
+        var dbo = db.db("veritabanim")
+        dbo.collection("collectionum").find({}).toArray((err,result)=>{
+            if(err) throw err
+            console.log(result);
+            db.close();
+        })
+    })
+    res.render("index");
 })
 
+const ekleme = ({data}) => {
+    MongoClient.connect(
+        uri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err,db)=>{
+            if(err) throw err
+            var dbo = db.db("veritabanim")
+            dbo.collection("collectionum").insertOne({todoApp:data},(err,res)=>{
+                if (err) throw err;
+                console.log("veri kayıt işlemi başarılı");
+                console.log(data);
+                db.close();
+            })
+        })
+}
 app.post("/",(req,res)=>{
-    yapilacaklar.push(req.body.gorev);
-    res.render("index",{data:yapilacaklar});
+    ekleme({data:req.body.gorev});
+    res.render("index");
 
 })
 
